babbe42f737ed1e92a5915f9f82cac788f87b264
jovdkerc <joshua_vdk@hotmail.com>
2022-04-05 15:19:56 +0400

    Initial commit

---
+++ coingecko_search.py
@@ -0,0 +1,9 @@
+'''This script scrapes coingecko coins and stores coins that are up 20-50% on the day
+or over 50% on the week. We need this info to establish which coins are potentially in an uptrend
+'''
+
+
+class Searcher:
+    def search_coingecko(self):
+
+if __name__ == "__main__":

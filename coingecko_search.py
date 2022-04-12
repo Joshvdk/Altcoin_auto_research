22ae5d3964dce38a4754e46ddfa2faf106c377ba
jovdkerc <joshua_vdk@hotmail.com>
2022-04-12 09:52:52 +0400

    Initial commit

--- coingecko_search.py
+++ coingecko_search.py
@@ -2,8 +2,20 @@
 or over 50% on the week. We need this info to establish which coins are potentially in an uptrend
 '''
 
-
+from pycoingecko import CoinGeckoAPI
+from bs4 import BeautifulSoup
 class Searcher:
+
+    def __init__(self):
+        self.cgk = CoinGeckoAPI()
     def search_coingecko(self):
+        for coin in self.cgk.get_search_trending()['coins']:
+            print(coin['item'])
+
+    def scraped_search(self):
+        self.soup = BeautifulSoup(page.content,"html.parser")
+
 
 if __name__ == "__main__":
+    searcher = Searcher()
+    searcher.search_coingecko()
